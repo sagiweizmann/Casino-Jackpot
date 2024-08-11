a05887a9b929e4a2c800ae086f7095199e60b37c
sagiweizmann <sagiweizmann@gmail.com>
2024-08-11 13:08:01 +0300

    Slots controller session start and credits init

--- src/Controller/SlotsController.php
+++ src/Controller/SlotsController.php
@@ -2,13 +2,28 @@
 
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SlotsController extends AbstractController {
+    private const SYMBOLS = ['🍒', '🍋', '🍊', '🍉'];
+    private const REWARDS = [
+        '🍒' => 10,
+        '🍋' => 20,
+        '🍊' => 30,
+        '🍉' => 40
+    ];
     #[Route('/')]
     public function main(): Response
     {
         return $this->render('slots/index.html.twig');
+    }
+    #[Route('/start', name: 'start_game', methods: ['POST'])]
+    public function startGame(SessionInterface $session): JsonResponse
+    {
+        $session->set('credits', 10);
+        return new JsonResponse(['credits' => 10]);
     }
 }
